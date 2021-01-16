1e27f256e1f3e3951663d79798b277b11babf12b
Ricky Badhai <ricky.badhai9852.com>
2021-01-16 20:00:50 +0530

    add extra weather details

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,8 @@ const forecast=(latitude,longitude,callback)=>{
 		callback('unable to find',undefined)
 	}
 	else{
-		callback(undefined,'Current weather condition is '+ body.current.condition.text +'.It is currently '+ body.current.temp_c +' degrees of Celsius out. There is a ' + body.current.precip_mm + '% chance of rain.')
+		console.log(body.current)
+		callback(undefined,'Current weather condition is '+ body.current.condition.text +'.It is currently '+ body.current.temp_c +' degrees of Celsius out. There is a ' + body.current.precip_mm + '% chance of rain.'+'Wind pressure is:'+body.current.pressure_mb+' milibar.'+'Wind speed is:'+body.current.wind_kph+' kmph.'+'Humidity is:'+body.current.humidity+' %.'+'UV Index is:'+body.current.uv)
 
 	}
 
